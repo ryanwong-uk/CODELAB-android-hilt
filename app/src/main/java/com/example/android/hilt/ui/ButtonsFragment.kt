16e8b24339dbc662c74c79e4c041d4d0aedca661
Ryan Wong <ryan@rwmobi.com>
2021-10-13 00:14:38 +0100

    [feature] App (step 9): create LoggerDataSource and LoggerInMemoryDataSource; add LoggingModule for fragments to use the LoggerInMemoryDataSource

--- app/src/main/java/com/example/android/hilt/ui/ButtonsFragment.kt
+++ app/src/main/java/com/example/android/hilt/ui/ButtonsFragment.kt
@@ -23,7 +23,8 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.android.hilt.R
-import com.example.android.hilt.data.LoggerLocalDataSource
+import com.example.android.hilt.data.LoggerDataSource
+import com.example.android.hilt.di.InMemoryLogger
 import com.example.android.hilt.navigator.AppNavigator
 import com.example.android.hilt.navigator.Screens
 import dagger.hilt.android.AndroidEntryPoint
@@ -35,8 +36,12 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class ButtonsFragment : Fragment() {
 
-    @Inject lateinit var logger: LoggerLocalDataSource
-    @Inject lateinit var navigator: AppNavigator
+    @InMemoryLogger
+    @Inject
+    lateinit var logger: LoggerDataSource
+
+    @Inject
+    lateinit var navigator: AppNavigator
 
     override fun onCreateView(
         inflater: LayoutInflater,
